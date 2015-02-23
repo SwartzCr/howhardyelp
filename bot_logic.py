f26997eaf0305af676c5d5b2590b5a27a8fd6f89
Noah Swartz <swartzcr@gmail.com>
2015-02-23 00:13:06 -0800

    added some try's to deal with yelp not returning possible reviews

--- bot_logic.py
+++ bot_logic.py
@@ -20,7 +20,13 @@ def pick_loc(corpora):
     return loc["city"], loc["state"]
 
 def get_tweet_text(review_snippet):
-    punc = [i for (i, x) in enumerate(review_snippet.splitlines()[0]) if x in ["!", ".", "?", "\n"]]
+    segment = ""
+    snip = review_snippet.splitlines()
+    for item in snip:
+        if len(item) > 0:
+            segment = item
+            break
+    punc = [i for (i, x) in enumerate(segment) if x in ["!", ".", "?", "\n"]]
     for place in punc[::-1]:
         if place <= 139:
             return review_snippet[:place+1]
@@ -32,10 +38,16 @@ def prep_tweet():
     city, state = pick_loc(corpora)
     loc_url = yelp_search.urlify_location(city, state)
     loc_resp = requests.get(loc_url)
-    biz_snippet = yelp_search.get_biz_from_location_query(loc_resp)
+    try:
+        biz_snippet = yelp_search.get_biz_from_location_query(loc_resp)
+    except:
+        return ""
     biz_url = yelp_search.urlify_biz(biz_snippet)
     biz_resp = requests.get(biz_url)
-    review_snippet = yelp_search.get_review_snippet(biz_resp)
+    try:
+        review_snippet = yelp_search.get_review_snippet(biz_resp)
+    except:
+        return ""
     return get_tweet_text(review_snippet)
 
 def main():
@@ -43,7 +55,7 @@ def main():
     tweet = ""
     while len(tweet) < 1:
         tweet = prep_tweet()
-    twitter.update_status(status=tweet)
-    #print tweet
+   # twitter.update_status(status=tweet)
+    print tweet
 
 main()
